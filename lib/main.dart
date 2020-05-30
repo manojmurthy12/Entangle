5359d7dfacb3cdabe336ba513c59085ca1dbe6f3
Manoj P <manoj1000manoj@gmail.com>
2020-05-30 19:25:15 +0530

    preferences page should be set up

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
 
+int _semester = 0;
+int _branch = 0;
 void main() {
   runApp(MaterialApp(home: Splash()));
 }
@@ -29,11 +31,45 @@ class _SplashState extends State<Splash> {
   }
 }
 
-class FirstScreen extends StatelessWidget {
+class FirstScreen extends StatefulWidget {
+  @override
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  Scaffold preferences() {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Select your Preferences'),
+      ),
+      body: Container(
+        color: Colors.grey,
+        child: Center(
+          child: Column(
+            children: [
+              RaisedButton(
+                  onPressed: () {
+                    setState(() {
+                      _semester = 1;
+                    });
+                  },
+                  child: Text('sem1')),
+              RaisedButton(
+                  onPressed: () {
+                    setState(() {
+                      _semester = 1;
+                    });
+                  },
+                  child: Text('sem2')),
+            ],
+          ),
+        ),
+      ),
+    );
+  } //make sure to store the preferences in a seperate variable
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.grey,
-    );
+    return preferences(); //edit this 'preferences' function with drop down box and stuff...
   }
 }
